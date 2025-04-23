6c3e243062a72b4b8c19d455f5a6c4ae3e72ac32
kimminu7 <rock9024@naver.com>
2025-04-23 12:49:37 +0900

    feat : 회원가입 기능 추가 ( POST )
    email, password 예외처리 ( 정규표현식적용 )

---
+++ src/main/java/org/example/outsourcing_project/domain/user/entity/User.java
@@ -0,0 +1,45 @@
+package org.example.outsourcing_project.domain.user.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.example.outsourcing_project.common.entity.BaseTimeEntity;
+
+@Getter
+@Entity
+@Table(name = "user")
+public class User extends BaseTimeEntity {
+
+    // PK
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 이메일
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    // 비밀번호
+    @Column(nullable = false)
+    private String password;
+
+    // 이름
+    @Column(nullable = false)
+    private String name;
+
+    // 주소
+    @Column(nullable = false)
+    private String address;
+
+    // 역할/권한
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserRole role;
+
+    public User(String email, String password, String name, String address, UserRole role) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.address = address;
+        this.role = role;
+    }
+}
