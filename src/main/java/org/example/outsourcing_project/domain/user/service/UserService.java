7fc98348922bce6c7eaed832f543b71e05cc44c3
kimminu7 <rock9024@naver.com>
2025-04-23 14:40:37 +0900

    feat : 전체 유저 조회 기능 추가 ( GET )
    주석으로 설명 추가

--- src/main/java/org/example/outsourcing_project/domain/user/service/UserService.java
+++ src/main/java/org/example/outsourcing_project/domain/user/service/UserService.java
@@ -1,9 +1,13 @@
 package org.example.outsourcing_project.domain.user.service;
 
-import org.example.outsourcing_project.domain.user.dto.SignupUserResponseDto;
+import org.example.outsourcing_project.domain.user.dto.response.UserResponseDto;
 import org.example.outsourcing_project.domain.user.entity.UserRole;
+
+import java.util.List;
 
 public interface UserService {
 
-    SignupUserResponseDto Signup(String email, String password, String name, String address, UserRole role);
+    UserResponseDto Signup(String email, String password, String name, String address, UserRole role);
+
+    List<UserResponseDto> findAll();
 }
