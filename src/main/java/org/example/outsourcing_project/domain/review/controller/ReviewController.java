04e9502dc26fba0502af112ebc589cf4c33e685b
SaltBr <snowgriff123@gmail.com>
2025-04-23 14:35:42 +0900

    fix : 리뷰 작성 기능 수정
    - 로그인된 유저와 주문한 유저가 같은지 체크

--- src/main/java/org/example/outsourcing_project/domain/review/controller/ReviewController.java
+++ src/main/java/org/example/outsourcing_project/domain/review/controller/ReviewController.java
@@ -9,6 +9,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/reviews")
@@ -17,13 +19,24 @@ public class ReviewController {
 
     @PostMapping("/{orderId}")
     public ResponseEntity<ReviewResponseDto> createReview (
+            @SessionAttribute("user") Long userId,
             @PathVariable Long orderId,
             @Valid @RequestBody ReviewRequestDto requestDto) {
-        ReviewResponseDto reviewResponseDto = reviewService.createReview(orderId, requestDto);
+        ReviewResponseDto reviewResponseDto = reviewService.createReview(userId, orderId, requestDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(reviewResponseDto);
     }
 
-    //@GetMapping
+    //리뷰 조회
+    //최신순 OR 오래된순
+    //별점 범위 지정
+    @GetMapping
+    public ResponseEntity<List<ReviewResponseDto>> getReviewsByFilter (
+            @RequestParam(required = false, defaultValue = "1") int min,
+            @RequestParam(required = false, defaultValue = "5") int max,
+            @RequestParam(required = false, defaultValue = "newest") String order ){
+        List<ReviewResponseDto> result = reviewService.getReviewsByFilter(min, max, order);
+        return ResponseEntity.ok(result);
+    }
 
     //@PatchMapping
 
--- src/main/java/org/example/outsourcing_project/domain/review/service/ReviewService.java
+++ src/main/java/org/example/outsourcing_project/domain/review/service/ReviewService.java
@@ -10,6 +10,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class ReviewService {
@@ -18,20 +20,30 @@ public class ReviewService {
 
     //리뷰 작성
     @Transactional
-    public ReviewResponseDto createReview(Long orderId, ReviewRequestDto requestDto){
+    public ReviewResponseDto createReview(Long currentUserId, Long orderId, ReviewRequestDto requestDto){
         Order order = orderRepository.findById(orderId)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "주문을 찾을 수 없습니다."));
 
-        Review review = new Review(order, requestDto.getContents(), requestDto.getStars());
-        Review savedReview = reviewRepository.save(review);
+        //현재 로그인된 유저와 주문의 유저가 같다면 리뷰 저장
+        if(currentUserId.equals(order.getUser().getUserId())){
+            Review review = new Review(order, requestDto.getContents(), requestDto.getStars());
+            Review savedReview = reviewRepository.save(review);
 
-        return ReviewResponseDto.builder()
-                .contents(savedReview.getContents())
-                .stars(savedReview.getStars())
-                .userName(order.getUser().getName())
-                .createdAt(savedReview.getCreatedAt())
-                .updatedAt(savedReview.getUpdatedAt())
-                .orderMenu(order.getMenu().getMenuName())
-                .build();
+            return ReviewResponseDto.builder()
+                    .contents(savedReview.getContents())
+                    .stars(savedReview.getStars())
+                    .userName(order.getUser().getName())
+                    .createdAt(savedReview.getCreatedAt())
+                    .updatedAt(savedReview.getUpdatedAt())
+                    .orderMenu(order.getMenu().getMenuName())
+                    .build();
+        }
+
+        throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "본인의 주문에만 리뷰를 작성할 수 있습니다.");
+    }
+
+    //리뷰 조회
+    public List<ReviewResponseDto> getReviewsByFilter(int min, int max, String order) {
+
     }
 }
