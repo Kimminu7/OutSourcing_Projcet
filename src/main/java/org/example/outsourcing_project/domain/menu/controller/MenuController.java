1b97dd58d80c745ca4d263aeec276ee64e001706
isom <smallsol.fa@gmail.com>
2025-04-23 14:45:01 +0900

    feat : 메뉴 관련 기본 클래스 및 인터페이스 생성

---
+++ src/main/java/org/example/outsourcing_project/domain/menu/controller/MenuController.java
@@ -0,0 +1,4 @@
+package org.example.outsourcing_project.domain.menu.controller;
+
+public class MenuController {
+}
---
+++ src/main/java/org/example/outsourcing_project/domain/menu/dto/request/MenuRequestDto.java
@@ -0,0 +1,4 @@
+package org.example.outsourcing_project.domain.menu.dto.request;
+
+public class MenuRequestDto {
+}
