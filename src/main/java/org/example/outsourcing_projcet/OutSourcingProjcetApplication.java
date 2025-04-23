e4c268506ccc2e150ae36287486f4022b056a03c
kimminu7 <rock9024@naver.com>
2025-04-23 10:23:00 +0900

    Initial commit

---
+++ src/main/java/org/example/outsourcing_projcet/OutSourcingProjcetApplication.java
@@ -0,0 +1,13 @@
+package org.example.outsourcing_projcet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutSourcingProjcetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OutSourcingProjcetApplication.class, args);
+    }
+
+}
