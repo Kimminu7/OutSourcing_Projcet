82e7f6bcc048fbfbe1253bacf40ca0950db12d6d
isom <smallsol.fa@gmail.com>
2025-04-24 01:20:10 +0900

    feat : 메뉴 생성 기능 추가

--- src/main/java/org/example/outsourcing_project/domain/menu/repository/MenuRepository.java
+++ src/main/java/org/example/outsourcing_project/domain/menu/repository/MenuRepository.java
@@ -1,4 +1,7 @@
 package org.example.outsourcing_project.domain.menu.repository;
 
-public interface MenuRepository {
+import org.example.outsourcing_project.domain.menu.entity.Menu;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MenuRepository extends JpaRepository<Menu, Long> {
 }
