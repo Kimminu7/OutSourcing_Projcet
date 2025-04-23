d5e086d636420887794d11fe118794bd0cecc26b
rabitis99 <kyungsu9914@gmail.com>
2025-04-23 11:38:08 +0900

    :sparkles: feat : 가게 entity 및 기본 클래스 골조

---
+++ src/main/java/org/example/outsourcing_project/domain/shop/controller/ShopController.java
@@ -0,0 +1,11 @@
+package org.example.outsourcing_project.domain.shop.controller;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping
+@RestController
+public class ShopController {
+}
---
+++ src/main/java/org/example/outsourcing_project/domain/shop/service/ShopService.java
@@ -0,0 +1,4 @@
+package org.example.outsourcing_project.domain.shop.service;
+
+public class ShopService {
+}
