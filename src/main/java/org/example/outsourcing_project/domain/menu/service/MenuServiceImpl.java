111e830987c332dd1a2e4aa80eedf21a1211ff59
isom <smallsol.fa@gmail.com>
2025-04-24 15:13:02 +0900

    feat : 메뉴 업데이트 기능 추가

--- src/main/java/org/example/outsourcing_project/domain/menu/service/MenuServiceImpl.java
+++ src/main/java/org/example/outsourcing_project/domain/menu/service/MenuServiceImpl.java
@@ -1,12 +1,15 @@
 package org.example.outsourcing_project.domain.menu.service;
 
 import org.example.outsourcing_project.domain.menu.dto.request.MenuCreateRequestDto;
+import org.example.outsourcing_project.domain.menu.dto.request.MenuUpdateRequestDto;
 import org.example.outsourcing_project.domain.menu.dto.response.MenuCreateResponseDto;
+import org.example.outsourcing_project.domain.menu.dto.response.MenuUpdateResponseDto;
 import org.example.outsourcing_project.domain.menu.entity.Menu;
 import org.example.outsourcing_project.domain.menu.repository.MenuRepository;
 import org.example.outsourcing_project.domain.shop.entity.Shop;
 import org.example.outsourcing_project.domain.shop.repository.ShopRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 
@@ -22,13 +25,31 @@ public class MenuServiceImpl implements MenuService{
 	@Override
 	public MenuCreateResponseDto createMenu(Long userId,Long shopId,MenuCreateRequestDto requestDto) {
 
-		// TODO shop 생성 후 수정 필요
-		Shop shop = shopRepository.findById(shopId).
-			orElseThrow(() -> new IllegalArgumentException("존재하지 않는 매장입니다."));
+		// TODO 수정 필요
+		Shop shop = shopRepository.findById(shopId)
+			.orElseThrow(() -> new IllegalArgumentException("존재하지 않는 매장입니다."));
 
 		Menu saveMenu = new Menu(shop,requestDto);
 		menuRepository.save(saveMenu);
 
 		return new MenuCreateResponseDto(saveMenu);
+	}
+
+	@Override
+	@Transactional
+	public MenuUpdateResponseDto updateMenu(Long userId, Long shopId,Long menuId, MenuUpdateRequestDto requestDto) {
+
+		// TODO 수정 필요
+		Shop shop = shopRepository.findById(shopId)
+			.orElseThrow(() -> new IllegalArgumentException("존재하지 않는 매장입니다."));
+
+
+		Menu menu = menuRepository.findByIdAndShop_ShopId(menuId,shopId)
+				.orElseThrow(()-> new IllegalArgumentException("메뉴가 존재하지 않습니다."));
+
+
+		menu.update(requestDto);
+
+		return new MenuUpdateResponseDto(menu);
 	}
 }
